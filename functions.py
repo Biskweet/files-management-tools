4d18de29dbb964f30dc419bfec8bc727a465b7fb
Biskweet <gaspard.dama@gmail.com>
2024-02-25 03:16:24 +0100

    NEw function 'walk'

--- functions.py
+++ functions.py
@@ -20,13 +20,17 @@ def get_all_extensions(directory):
 	files, folders = get_dir_content(directory)
 	return list(set([os.path.splitext(directory + "/" + file)[1].lower() for file in files] + sum([get_all_extensions(directory + "/" + dir) for dir in folders], start=[])))
 
-# humanize.filesize.naturalsize(dir_size("."))
-
 
 # --- Find all path with given extension ---
 def find_extension_occ(directory, extension):
 	files, folders = get_dir_content(directory)
 	return list(filter(lambda filename: os.path.splitext(directory + "/" + filename)[1].lower()[1:] == extension.replace(".", ""), map(lambda f: directory + "/" + f, files))) + sum([find_extension_occ(directory + "/" + folder, extension) for folder in folders], start=[])
+
+
+# --- e ---
+def get_all_filenames_with_given_extension(directory, extensions):
+	files, folders = get_dir_content(directory)
+	return list(set([os.path.splitext(directory + "/" + file)[0].lower() for file in files if os.path.splitext(directory + "/" + file)[1].lower() in extensions] + sum([get_all_extensions(directory + "/" + dir) for dir in folders], start=[])))
 
 
 # --- Batch rename (replaces spaces with underscores) ---
@@ -40,3 +44,26 @@ def renamer(directory, previous_string, new_string):
             os.rename(directory + '/' + folder, directory + '/' + new_folder_name)
             renamer(directory + '/' + new_folder_name)
         except: ...
+
+
+# --- Get total size of an open directory ---
+def walk(path):
+	global count
+	if locals().get("count") == None:
+            count = 0
+	count += 1
+	print("  ---", count, end="\r")
+	if path[-1] != '/':
+		path += '/'
+	response = r.get(path)
+	soup = BeautifulSoup(response.text)
+	strings = list(map(lambda string: re.sub(r"\s+", " ", string.strip()), list(soup.body.strings)))
+	strings = strings[4:]
+	s = 0
+	links = []
+	for i in range(0, len(strings), 2):
+		links.append(strings[i])
+		val = strings[i+1].split(" ")[-1]
+		if val.isdigit():
+			s += int(val)
+	return s + sum([walk(path + link) if link[-1] == '/' else 0 for link in links])
